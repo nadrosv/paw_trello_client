fa4d1c17c18980f69d6a3f04faebf0e14bd1efbe
korSt <k4z1kpl@gmail.com>
2016-10-20 00:19:34 +0200

    List component styling

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,28 @@
 <template>
 	<!--<div class="col-md-8">-->
 		<div class="col-md-4 list-area">
-			<span>
-             ID: {{listData.id}} 
-            </span>
-			<span class="delete-list" v-on:click="del">X</span>
+			
+      <span v-if="listEditable">
+            <input v-model="listName">
+            <button class="btn btn-default" v-on:click="saveList">
+              <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+            </button>
+            <!--<p>Message is: {{ message }}</p>-->
+        </span>
+			<span v-else>
+        {{listData.list_name}} 
+        <div class="btn-group" role="group" aria-label="listButtons">
+            <button class="btn btn-default" v-on:click="addCard">
+              <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+            </button>
+            <button class="btn btn-default" v-on:click="editList">
+              <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+            </button>
+            <button class="btn btn-default" v-on:click="delList">
+              <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+            </button>  
+        </div>    
+        <span>
 			<div>
 				<card v-for="card in cards" :card-data="card"></card>
 			</div>
@@ -15,23 +33,42 @@
   export default {
     data() {
       return {
-          
-        cards: []
+        cards: [],
+        listEditable: false,
+        listName: ''
+
       }
     },
     methods: {
       getCard() {
         this.$http.get('http://localhost:3000/cards?listId='+this.listData.id).then((response) => {
-         
           this.cards = response.body;
         }, (response) => {
          console.log(response)
         });
     },
-    del() {
+    addCard() {
+      
+    },
+    delList() {
         this.$el.remove();
        //this.$emit('del')
+    },
+    editList() {
+        console.log('edit')
+        this.listEditable = true
+    },
+    saveList() {
+        console.log('saving')
+        this.listData.list_name = this.listName
+        this.listEditable = false
+        this.$http.put('http://localhost:3000/lists/' + this.listData.id, this.listData ).then((response) => {
+        console.log(response.body)
+        }, (response) => {
+         console.log(response)
+        });
     }
+     
   },
    //created: function () {
      //   this.getCard();
