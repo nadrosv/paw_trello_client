4a9e269d504a658041ad96b00a38ba9f1301f1f2
korSt <k4z1kpl@gmail.com>
2016-10-19 21:45:32 +0200

    Get boards for logged user

--- src/auth/index.js
+++ src/auth/index.js
@@ -7,18 +7,21 @@ const SIGNUP_URL = API_URL + 'users/'
 export default {
 
     user: {
-        authenticated: false
+        authenticated: false,
+        id: 0
     },
 
     login(context, creds, redirect) {
         context.$http.get('http://localhost:3000/users?userName=' + creds.username + '&password=' + creds.password).then((response) => {
             console.log(response)
-            if (response.body.length) {
+            if (response.body.length > 0) {
                 this.user.authenticated = true
+                console.log(response.body[0].id)
+                this.user.id = response.body[0].id
                 localStorage.setItem('id_token', response.body[0].id)
                 
                 // if (redirect) {
-                    router.go(redirect)
+                    // router.go(redirect)
                 // }
             } else {
                 context.error = 'Brak takiego uzytkownika'
@@ -52,6 +55,7 @@ export default {
         var jwt = localStorage.getItem('id_token')
         if (jwt) {
             this.user.authenticated = true
+            this.user.id = jwt
         }
         else {
             this.user.authenticated = false
