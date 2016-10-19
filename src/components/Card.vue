ceb2a574352823e23758da47b2535bd77dc8b913
korSt <k4z1kpl@gmail.com>
2016-10-20 00:59:53 +0200

    Added missing functionality to Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,16 +3,42 @@
 	<div class="card-area">
 		<!--ID: {{cardData.id}}-->
 		<span>
-      cardData.card_name
+      {{cardData.card_name}}
     </span>
       <div class="btn-group" role="group" aria-label="listButtons">
-    <button class="btn btn-default" v-on:click="editList">
+    <button class="btn btn-default" data-toggle="modal" data-target="#cardModal">
       <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
     </button>
-    <button class="btn btn-default" v-on:click="delList">
+    <button class="btn btn-default" v-on:click="delCard">
       <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
     </button>  
         </div>   
+
+         <!--Modal-->
+			<div class="modal fade" id="cardModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+							<h4 class="modal-title" id="myModalLabel">Dodaj nowa karte</h4>
+						</div>
+						<div class="modal-body">
+							<p>
+								Nazwa
+								<input v-model="cardData.card_name">
+                Opis
+								<input v-model="cardData.desc">
+
+							</p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary" v-on:click="editCard">Save changes</button>
+						</div>
+					</div>
+				</div>
 	</div>
 </template>
 
@@ -20,13 +46,24 @@
   export default {
     data() {
       return {
-        cards: []
+        "editedCard": {}
       }
     },
     methods: {
-        del() {
-        this.$el.remove();
-       //this.$emit('del')
+    editCard() {
+      this.$http.put('http://localhost:3000/cards/' + this.cardData.id, this.cardData ).then((response) => {
+        console.log(response.body)
+        }, (response) => {
+         console.log(response)
+        });
+    },
+    delCard() {
+      this.$el.remove();
+      this.$http.delete('http://localhost:3000/cards/' + this.cardData.id).then((response) => {
+        console.log(response.body)
+        }, (response) => {
+         console.log(response)
+        });
     }
      
   },
